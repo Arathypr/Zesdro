e2566e6820cc42bf6aa976d2609f5feef9f8c445
Arathy <arathyp.radhakrishnan@gmail.com>
2023-07-30 12:13:32 +0530

    full responsive

---
+++ src/Components/Circle/Circle.jsx
@@ -0,0 +1,73 @@
+import './Circle.css'
+
+
+function Circle(){
+    return(
+       <div className="rectangle-circle">
+            <div className="no-fuzz-no-container">
+            <p className="no-fuzz-no">NO FUZZ, NO BULLSHIT</p>
+            <p className="services-done-on">SERVICES DONE ON TRUST</p>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col">    
+                    <div className="big-circle">
+                        <div className="circle1 position-absolute">
+                            <p className='data1'>Digital <br/>Marketing</p>
+                        </div>
+                        <div className="circle2 position-absolute">
+                            <p className='data2 '>User <br/>Experience <br/>Design</p>
+                        </div>
+                        <div className="circle3 position-absolute">
+                            <p className='data3'>Brand<br/>Identity</p>
+                        </div>
+                        <div className="circle4 position-absolute">
+                            <p className='data4'>Website<br/>Devlopment</p>
+                        </div>
+                        <div className="circle5 position-absolute">
+                            <p className='data5'>SEO-Search<br/>Engine<br/>Optimization</p>
+                        </div>
+                        <div className="circle6 position-absolute">
+                            <p className='data6'>Custom CMS<br/>& Wordpress</p>
+                        </div>
+                        <div className="circle7 position-absolute">
+                            <p className='data7'>Branding <br/>Design</p>
+                        </div>
+                        <div className="circle8 position-absolute">
+                            <p className='data8'>App <br/>Development<br/>IOS & Android</p>
+                        </div>
+                        <div className="circle9 position-absolute">
+                            <p className='data9'>User Interface<br/>Design</p>
+                        </div>
+                        <div className="circle10 position-absolute">
+                            <p className='data10'>Business<br/>Strategy</p>
+                        </div>
+                    </div>
+                    
+                    </div>
+                    <div className="col">
+                        <div className="d-flex flex-column  justify-content-end align-items-end btn-lg main-button col">      
+                        <div>
+                        <button type="button" class="btn btn-outline-light mb-5 mt-5 custom-btn">HEALTHCARE</button>
+                        </div>
+                        <div>
+                        <button type="button" class="btn btn-outline-light mb-5 custom-btn">EGINEERING</button>
+                        </div>
+                        <div>
+                        <button type="button" class="btn btn-outline-light mb-5 custom-btn">E-COMMERCE</button>
+                        </div>
+                        <div>
+                        <button type="button" class="btn btn-outline-light mb-5 custom-btn">TRAVEL & HOSPITALITY</button>
+                        </div>
+                        <div>
+                        <button type="button" class="btn btn-outline-light mb-5 custom-btn">DIGITAL MARKETING</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+       </div> 
+    );
+}
+export default Circle;
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,65 +1,72 @@
-import './Footer.css'
-import Groupdot from "../../assets/Groupdot.png"
-import Union from "../../assets/Union.png"
+import "./Footer.css";
+import Groupdot from "../../assets/Groupdot.png";
+import Union from "../../assets/Union.png";
 
-function Footer(){
-    return(
-     <div className="rectangle-div3 container-fluid">
-      <div className='position-relative'> <img src={Groupdot} className=" dots1 position-absolute bottom-0 end-0" /></div>
-        <div className="have-a-cryptic-container">
-          <p className="have-a-cryptic-project-with-yo">
-            <span>Have a </span>
-            <span className="have-a">cryptic</span>
-            <span> project with </span>
-            <span className="have-a">you</span>
-            <span> ?</span>
-          </p>
-          <p className="well-make-it-bright">
-            <span>We’ll make it</span> <span className="have-a">bright</span>
-          </p>
+function Footer() {
+  return (
+    <div className="rectangle-div3 container-fluid">
+      <div className="position-relative">
+        {" "}
+        <img
+          src={Groupdot}
+          className=" dots1 position-absolute bottom-0 end-0"
+        />
+      </div>
+      <div className="have-a-cryptic-container">
+        <p className="have-a-cryptic-project-with-yo">
+          <span>Have a </span>
+          <span className="have-a">cryptic</span>
+          <span> project with </span>
+          <span className="have-a">you</span>
+          <span> ?</span>
+        </p>
+        <p className="well-make-it-bright">
+          <span>We’ll make it</span> <span className="have-a">bright</span>
+        </p>
+      </div>
+      <div className="feel-free-to">
+        Feel free to reach out if you want to collaborate with us, or simply
+        have a chat.
+      </div>
+      <div class="d-flex flex-row mb-3 imagecontainer mt justify-content-evenly">
+        <div class="p-2 logo">Zesdro_Logo</div>
+        <div class="p-2">
+          <img src={Union} alt="" className="img" />p
         </div>
-        <div className="feel-free-to">
-          Feel free to reach out if you want to collaborate with us, or simply have a
-          chat.
+      </div>
+      <div class="d-flex flex-row mb-3 imagecontainer mt justify-content-evenly">
+        <div class="p-2 logo">Zesdro.com</div>
+        <div class="p-2 ">
+          <img src={Union} alt="" className="img" />
         </div>
-        <div class="d-flex flex-row mb-3 imagecontainer mt justify-content-evenly">
-            <div class="p-2 logo">Zesdro_Logo</div>
-            <div class="p-2"><img src={Union} alt="" className='img'/>
-            p
-            </div>    
-        </div>
-        <div class="d-flex flex-row mb-3 imagecontainer mt justify-content-evenly">
-            <div class="p-2 logo">Zesdro.com</div>
-            <div class="p-2 "><img src={Union} alt="" className='img'/></div>    
-        </div>
-        <div className="rectangle-div-footer container-fluid">
-            <footer>
-              <hr  className='border border-white'/>
-              <div className="navbar">
-                <div className="celozisall-rights-reserved">
-                  @CELOZIS.All Rights Reserved
-                </div>
-                <nav>
-                  <ul>
-                    <li>
-                      <a href="#">Instagram</a>
-                    </li>
-                    <li>
-                      <a href="#">Facebook</a>
-                    </li>
-                    <li>
-                      <a href="#">LinkedIn</a>
-                    </li>
-                    <li>
-                      <a href="#">twitter</a>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
-            </footer>
+      </div>
+      <div className="rectangle-div-footer container-fluid">
+        <footer>
+          <hr className="border border-white" />
+          <div className="navbar">
+            <div className="celozisall-rights-reserved">
+              @CELOZIS.All Rights Reserved
+            </div>
+            <nav className="nav">
+              <ul>
+                <li>
+                  <a href="#">Instagram</a>
+                </li>
+                <li>
+                  <a href="#">Facebook</a>
+                </li>
+                <li>
+                  <a href="#">LinkedIn</a>
+                </li>
+                <li>
+                  <a href="#">twitter</a>
+                </li>
+              </ul>
+            </nav>
           </div>
+        </footer>
+      </div>
     </div>
-         
-    );
+  );
 }
 export default Footer;
